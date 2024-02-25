5c40910132988811f990a346ff58863c07a6aaac
rh2103 <quittaalma@gmail.com>
2024-02-25 19:49:10 +0700

    if player is dead, it will go up to heaven lol

--- dead_to_heaven.py
+++ dead_to_heaven.py
@@ -1,13 +1,12 @@
 from pygta import *
 
 while True:
-    if GTA.Player.GetHealth()==0 and not GTA.Player.OnVehicle():
-        ppos=GTA.Player.GetPosition()
-        for i in range(1000):
-            if GTA.Player.GetHealth()==0:
-                ppos=GTA.Player.GetPosition()
-                GTA.Player.SetPosition([ppos[0],ppos[1],ppos[2]+0.05])
-                time.sleep(0.01)
-            else:
-                break
-    time.sleep(1.5)
+    if GTA.Player.GetHealth()==0:
+        if not GTA.Player.OnVehicle():
+            for i in range(1000):
+                if GTA.Player.GetHealth()==0:
+                    GTA.Player.SetZ(GTA.Player.GetZ()+0.035)
+                    time.sleep(0.01)
+                else:
+                    break
+    time.sleep(.5)
